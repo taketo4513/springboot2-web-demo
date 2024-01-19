e3a074a13b1c1f4e49b0ae21be2bd5a3dc735ff9
Zhang <taketo1015@foxmail.com>
2024-01-19 18:24:19 +0800

    Add: gradle build demo

---
+++ gradle-build-demo/src/test/java/cc/taketo/test/ExampleTest.java
@@ -0,0 +1,12 @@
+package cc.taketo.test;
+
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void test() {
+        System.out.println("Hello World!");
+    }
+}
