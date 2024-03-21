4ad78a0018eed041164d953ade5fc7fa4df50238
Zhang <taketo1015@foxmail.com>
2024-03-21 15:44:56 +0800

    Add: gauss db test

---
+++ data-gauss-demo/src/main/java/cc/taketo/Application.java
@@ -0,0 +1,15 @@
+package cc.taketo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Zhangp
+ * @date 2024/3/21 9:30
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
