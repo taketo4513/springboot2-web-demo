c4a69b6fc5a7394c40e257e37e21427944103ab8
zhangpeng <645311501@qq.com>
2024-04-21 16:45:53 +0800

    Add: prometheus demo

---
+++ monitor-prometheus-demo/src/main/java/cc/taketo/MonitorPrometheusApplication.java
@@ -0,0 +1,11 @@
+package cc.taketo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorPrometheusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorPrometheusApplication.class, args);
+    }
+}
