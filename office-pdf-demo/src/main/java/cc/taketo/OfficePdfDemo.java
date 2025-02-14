eec66bea1587944c2a9286b223f0cd994218045a
zhangp <taketo1015@foxmail.com>
2025-02-14 18:30:27 +0800

    add: edit pdf demo

---
+++ office-pdf-demo/src/main/java/cc/taketo/OfficePdfDemo.java
@@ -0,0 +1,16 @@
+package cc.taketo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Zhangp
+ * @date 2025/2/14 16:04
+ */
+@SpringBootApplication
+public class OfficePdfDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficePdfDemo.class, args);
+    }
+}
---
+++ office-pdf-demo/src/test/java/cc/taketo/ItextPdfTest.java
@@ -0,0 +1,16 @@
+package cc.taketo;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Zhangp
+ * @date 2025/2/14 16:26
+ */
+public class ItextPdfTest {
+
+    @Test
+    public void test() {
+
+
+    }
+}
